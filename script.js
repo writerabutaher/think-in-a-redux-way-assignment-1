cc505f3bf1816a491b3e81751603d77dc4cafb0f
Abu Taher <writerabutaher@gmail.com>
2023-11-18 14:16:51 +0600

    add new matches onClick

--- script.js
+++ script.js
@@ -1,1 +1,35 @@
-console.log("object");
+const allMatches = document.querySelector(".all-matches");
+const match = document.querySelector(".match");
+const addMatchBtn = document.querySelector(".lws-addMatch");
+let matchNumber = 2;
+
+const addMatch = () => {
+  const newMatch = document.createElement("div");
+  newMatch.classList.add("match");
+
+  newMatch.innerHTML = `
+    <div class="wrapper">
+        <button class="lws-delete">
+          <img src="./image/delete.svg" alt="" />
+        </button>
+        <h3 class="lws-matchName">Match ${matchNumber}</h3>
+      </div>
+      <div class="inc-dec">
+        <form class="incrementForm">
+          <h4>Increment</h4>
+          <input type="number" name="increment" class="lws-increment" />
+        </form>
+        <form class="decrementForm">
+          <h4>Decrement</h4>
+          <input type="number" name="decrement" class="lws-decrement" />
+        </form>
+      </div>
+      <div class="numbers">
+        <h2 class="lws-singleResult">0</h2>
+      </div>
+    `;
+
+  allMatches.appendChild(newMatch);
+
+  matchNumber++;
+};
